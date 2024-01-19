b7f0733bd63e453f02b7b2d4519a272ebe9bba8a
Vimal Kumar Mishra <vimalkumarm472@gmail.com>
2024-01-19 20:04:07 +0530

    19-01-24 20:04

---
+++ Java Winter PEP/day7n.java
@@ -0,0 +1,75 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+public class day7n {
+    public static void main(String[] args) {
+        // int arr[] = { 2, 3, 1, 2, 3 };
+        // Map<Integer, Integer> mp = new HashMap<>();
+        // for (int i = 0; i < arr.length; i++) {
+        // if (mp.containsKey(arr[i])) {
+        // mp.put(arr[i], mp.get(arr[i]) + 1);
+        // } else {
+        // mp.put(arr[i], 1);
+        // }
+        // }
+        // for (Map.Entry<Integer, Integer> entry : mp.entrySet()) {
+        // if (entry.getValue() > 1) {
+        // System.out.println(entry.getKey());
+        // }
+        // }
+
+        // int[] a = { 2, 3, 1, 2, 3, 2, 2, 4, 4, 4, 4 };
+        // HashSet<Integer> set = new HashSet<>();
+        // HashSet<Integer> dup = new HashSet<>();
+        // for (int value : a) {
+        // if (set.add(value) == false) {
+        // dup.add(value);
+        // }
+        // }
+        // for (int duplicate : dup) {
+        // System.out.println(duplicate);
+        // }
+
+        // double n = 25;
+        // System.out.println(Math.sqrt(n));
+    }
+}
+
+// class A{
+// }
+// enum Enum extends A{
+// ABC, BCD, CDE, DEF;
+// }
+// What will be the output of the code.
+// error
+
+// enum directions {
+// N, S, W, E;
+
+// private directions() {
+// System.out.println(1);
+// }
+// }
+
+// class mainc {
+// public static void main(String[] args) {
+// directions d1 = new directions();
+// }
+// }
+// What would be the output of the code on screen?
+// options
+// 1: A 1
+// 2: B Compilation error [correct answer is B]
+// 3: C Runtime Error
+// 4: D None of the above
+
+// What is the time complexity of HashMap.
+// options
+// 1: A O(n)
+// 2: B O(nlogn)
+// 3: C O(n*n)
+// 3: D none of these [correct answer is B, since it is O[1]]
+
+// what is the time complexity of finding square roots using Math.sqrt()
+// O(1)
